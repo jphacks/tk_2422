5e88a8254159f402af528e3ff4c0fda9df7e1da4
NobuhiroYokota <yokota.nobuhiro.job@gmail.com>
2024-10-26 18:20:23 +0900

    地域の詳細表示,動的ルーティング

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -1,5 +1,7 @@
 "use client";
+
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 import { Calendar } from "@/components/ui/calendar";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -29,8 +31,11 @@ export default function Component() {
   const [date, setDate] = useState<Date>();
   const [selectedRegion, setSelectedRegion] = useState<string | null>(null);
 
+  const router = useRouter();
+
   const handleRegionClick = (regionId: string) => {
     setSelectedRegion(regionId);
+    router.push(`/home/${regionId}`);
   };
 
   return (
