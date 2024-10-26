65f2f3e5f35f06a563078fced401b450af02a9d7
DaikiKakimi <4250ikiad@gmail.com>
2024-10-26 18:47:20 +0900

    import直し

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
 import { signInWithPopup } from "firebase/auth";
 import { auth, provider } from "../lib/firebase/firebase";
 import { db } from "../lib/firebase/firebase"; // Firestoreのインスタンスをインポート
@@ -8,11 +8,9 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
 import { motion } from "framer-motion";
-import { doc, setDoc } from "firebase/firestore"; // Firestoreのメソッド // Firestoreのメソッド
-import { useUser } from './context/UserContext';
+import { useUser } from "./context/UserContext";
 
 export default function SignIn() {
-
   const [username, setUsername] = useState("");
   const router = useRouter();
   const { updateUid } = useUser() || { updateUid: () => {} }; // useUserがnullの場合のデフォルト関数を提供
@@ -28,7 +26,7 @@ export default function SignIn() {
       });
       updateUid(user.uid);
       console.log("ユーザー情報がFirestoreに保存されました");
-      router.push('/home');
+      router.push("/home");
     } catch (error) {
       console.error("エラーが発生しました:", error);
     }
