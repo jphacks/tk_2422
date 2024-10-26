e5e0bcec5d636ed882421f6a86824bd98180cf0c
NobuhiroYokota <yokota.nobuhiro.job@gmail.com>
2024-10-26 14:51:52 +0900

    gmailログイン機能のテスト

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,27 @@
-export default function Home() {
+"use client"
+import { signInWithPopup } from "firebase/auth";
+import { auth, provider } from "../lib/firebase/firebase"
+
+function Home(){
   return (
     <>
-      <div>
-        test
-      </div>
+    <SignInButton/>
     </>
-  );
+  )
 }
+
+export default Home;
+
+function SignInButton() {
+
+  const signInWithGoogle = () =>{
+    signInWithPopup(auth,provider)
+  }
+
+  return (
+    <button onClick={signInWithGoogle}>
+      Googleでサインイン
+    </button>
+  )
+}
+
--- frontend/src/lib/firebase/firebase.ts
+++ frontend/src/lib/firebase/firebase.ts
@@ -1,15 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"
-import { GoogleAuthProvider } from "firebase/auth/web-extension";
+import { GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STRAGE_BUCKET,
-  messagingSenderId: process.env.MASSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
+  appId: process.env.NEXT_PUBLIC_APPID
 };
 
 // Initialize Firebase
